d54f1dd552c01fc1f99d18ad5d6b715ecdab4ee3
socratone <gim2origin@gmail.com>
2020-08-06 00:24:58 +0900

    익스포트 성공

--- public/Main.js
+++ public/Main.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  win.loadURL('http://localhost:3000/');
+  win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
 }
 app.on('ready', createWindow);
